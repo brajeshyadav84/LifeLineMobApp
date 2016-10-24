b61e66031be3d607f98113340e1664ad3f56b718
DESKTOP-NDVAEUT\Divyansh <brajesh.mymailbox@gmail.com>
2016-10-24 23:30:34 +0800

    update CSS and new service file

---
+++ www/js/constant.js
@@ -0,0 +1,24 @@
+////
+//// External URL
+////
+var URLS = {};
+
+URLS = {
+	getBloodBankDetails : "",
+	getHospitalDetails : ""
+}
+
+
+////
+////LIfeLIne API Use
+////
+var SERVER = {
+    LIfeLIneUrl: ''
+};
+
+var METHODS = {};
+
+METHODS = {
+	sendFeedback : '_ah/api/user/v1/send_feedback',
+	getReferenceCode : '_ah/api/user/v1/get_referencecode',
+}
---
+++ www/js/serverRequest.js
@@ -0,0 +1,38 @@
+angular.module('starter.services', [])
+
+    .service('lifeLineService',function($http,$q,$ionicLoading,$timeout){
+
+    	return {
+    		postLifeLineAPI: function (Request, serviceMethod) {
+                var deferred = $q.defer();
+                $ionicLoading.show({template: 'Loading...'});
+                var url = SERVER.LIfeLIneUrl + serviceMethod;
+                return $http.post(url, Request)
+                    .success(function (response) {
+                        $ionicLoading.hide();
+                        deferred.resolve(response);
+                    })
+                    .error(function (error) {
+                        $ionicLoading.hide();
+                        deferred.reject(error);
+                    });
+                return deferred.promise;
+            },
+
+            postExternalUrl: function (Request, serviceUrl) {
+                var deferred = $q.defer();
+                $ionicLoading.show({template: 'Loading...'});
+                var url = serviceUrl;
+                return $http.post(url, Request)
+                    .success(function (response) {
+                        $ionicLoading.hide();
+                        deferred.resolve(response);
+                    })
+                    .error(function (error) {
+                        $ionicLoading.hide();
+                        deferred.reject(error);
+                    });
+                return deferred.promise;
+            }
+    	}
+    })
