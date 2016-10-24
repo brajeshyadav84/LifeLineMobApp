f0b193e2127da2bcd286f9fce93b85931d3dc3d2
Nitesh Singh <sinnitesh@gmail.com>
2016-10-24 11:15:33 +0000

    added new Page blood bank

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,7 +13,32 @@ angular.module('starter.controllers', [])
 })
 
 .controller('BBListCtrl', function($scope) {
-	localStorage.setItem("isFirstTime", true);
+    $scope.groups = [];
+    for (var i = 0; i < 10; i++) {
+        $scope.groups[i] = {
+            name: i,
+            items: []
+        };
+        for (var j = 0; j < 1; j++) {
+            //$scope.groups[i].items.push(i + '-' + j);
+            $scope.groups[i].items.push("Blood Bank Description goes here...");
+        }
+    }
+
+    /*
+     * if given group is the selected group, deselect it
+     * else, select the given group
+     */
+    $scope.toggleGroup = function (group) {
+        if ($scope.isGroupShown(group)) {
+            $scope.shownGroup = null;
+        } else {
+            $scope.shownGroup = group;
+        }
+    };
+    $scope.isGroupShown = function (group) {
+        return $scope.shownGroup === group;
+    };
 });
 
 
