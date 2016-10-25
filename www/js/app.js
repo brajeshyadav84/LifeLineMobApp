667b801a359e47344a55ccc944597e2916eaaa9b
Nitesh Singh <sinnitesh@gmail.com>
2016-10-25 11:03:03 +0000

    Added Service for BB Bank

--- www/js/app.js
+++ www/js/app.js
@@ -23,10 +23,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 })
-.factory('Countries', function ($http) {
+.factory('BloodBankPage', function ($http) {
 
     var Countries = [];
     var Cities = [];
+    var BloodBanks = [];
 
 
         return {
@@ -40,15 +41,37 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             },
             getCities: function (id) {
                 return $http.get("http://api.geonames.org/childrenJSON?geonameId="+id+"&username=sinnitesh").then(function (response) {
-                    Countries = response;
-                    return Countries;
+                    Cities = response;
+                    return Cities;
+                }, function (error) {
+                    return error;
+                });
+            },
+            getbloodBanks: function (id) {
+                var res_id = "e16c75b6-7ee6-4ade-8e1f-2cd3043ff4c9";
+                var api_key = "9555f4b13e18327cb4a655f672c4fb37";
+                var filterColumnName = "district";
+                var filterColumnValue = id;
+                var fields = "state,city,district,h_name,address,pincode,contact,helpline,email,service_time";
+                var sortcolumnName = "district";
+                var offset="1";
+                var no_elements = "100";
+                var link = "http://data.gov.in/api/datastore/resource.json"
+                            +"?resource_id="+ res_id
+                            + "&api-key=" + api_key
+                            + "&filters[" + filterColumnName + "]=" + filterColumnValue
+                            + "&fields=" + fields
+                            + "&sort[" + sortcolumnName + "]=asc"
+
+                return $http.get(link).then(function (response) {
+                    BloodBanks = response;
+                    return BloodBanks;
                 }, function (error) {
                     return error;
                 });
             }
         }
     })
-
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,12 +12,16 @@ angular.module('starter.controllers', [])
     localStorage.setItem("isFirstTime", true);
 })
 
-.controller('BBListCtrl', function ($scope, $state, Countries) {
+.controller('BBListCtrl', function ($scope, $state, BloodBankPage) {
     $scope.groups = [];
     $scope.countries = [];
     $scope.cities = [];
+    $scope.bloodbankResult = {
+
+
+    };
     //Fetching Country List    
-    Countries.getCountries().then(function (response) {
+    BloodBankPage.getCountries().then(function (response) {
 
 
         for (var i = 0; i < response.data.totalResultsCount; i++) {
@@ -27,42 +31,88 @@ angular.module('starter.controllers', [])
             };
         }
 
-       
+
         for (var i = 0; i < response.data.totalResultsCount; i++) {
             $scope.countries[i] = {
                 name: response.data.geonames[i].name,
                 id: response.data.geonames[i].geonameId
-                };
+            };
         }
 
     });
     //Update Cities
     $scope.updateCities = function (city_id) {
-        console.log(city_id);     
-        Countries.getCities(city_id).then(function (response) {
+        BloodBankPage.getCities(city_id).then(function (response) {
             for (var i = 0; i < response.data.totalResultsCount; i++) {
                 $scope.cities[i] = {
                     name: response.data.geonames[i].name,
                     id: response.data.geonames[i].geonameId
                 };
             }
-            console.log($scope.cities);
+            $scope.groups = [];
+            //console.log($scope.cities);
         });
-    
+
     }
 
+    $scope.updatebloodBanks = function (objSelected) {
+        console.log(objSelected);
+        BloodBankPage.getbloodBanks(objSelected.ID.name).then(function (response) {
+            console.log(response.data);
+            if (response.data.success) {
+                for (var i = 0; i < response.data.count; i++) {
+
+                    $scope.groups[i] = {
+                        state: response.data.records[i].state,
+                        city: response.data.records[i].city,
+                        items: []
+                        //district: response.data.records[i].district,
+                        //h_name: response.data.records[i].h_name,
+                        //address: response.data.records[i].address,
+                        //contact: response.data.records[i].contact,
+                        //helpline: response.data.records[i].helpline,
+                        //pincode: response.data.records[i].pincode,
+                        //email: response.data.records[i].email,
+                        //service_time: response.data.records[i].service_time,
+
+
+                    };
+                    //for (var j = 0; j < 1; j++) {
+
+                        //$scope.groups[i].items.push("Blood Bank Description goes here...");
+                    $scope.groups[i].items.push({
+                        district: response.data.records[i].district,
+                        h_name: response.data.records[i].h_name ,
+                        address: response.data.records[i].address,
+                        contact: response.data.records[i].contact,
+                        helpline: response.data.records[i].helpline ,
+                        pincode: response.data.records[i].pincode,
+                        email: response.data.records[i].email,
+                        service_time: response.data.records[i].service_time,
+                    }
+                        //response.data.records[i]
+                        );
+                    //}
+
+                }
+            }
+            console.log($scope.groups.length);
+
+        });
+
+    }
     //Update list items
 
-    for (var i = 0; i < 10; i++) {
-        $scope.groups[i] = {
-            name: i,
-            items: []
-        };
-        for (var j = 0; j < 1; j++) {
-            //$scope.groups[i].items.push(i + '-' + j);
-            $scope.groups[i].items.push("Blood Bank Description goes here...");
-        }
-    }
+    //for (var i = 0; i < 10; i++) {
+    //    $scope.groups[i] = {
+    //        name: i,
+    //        items: []
+    //    };
+    //    for (var j = 0; j < 1; j++) {
+    //        //$scope.groups[i].items.push(i + '-' + j);
+    //        $scope.groups[i].items.push("Blood Bank Description goes here...");
+    //    }
+    //}
 
     /*
      * if given group is the selected group, deselect it
@@ -80,7 +130,7 @@ angular.module('starter.controllers', [])
     };
 });
 
-    
-    
+
+
 
 
