f93aeda613935e7ede90ae5b8fae94ad2e3abba3
DESKTOP-NDVAEUT\Divyansh <brajesh.mymailbox@gmail.com>
2016-11-20 13:29:49 +0800

    share app via social media

--- www/js/dashboardCtrl.js
+++ www/js/dashboardCtrl.js
@@ -1,6 +1,6 @@
 //angular.module('starter.controllers', [])
 
-LifeLine.controller('dashboardCtrl', function ($scope, $state) {
+LifeLine.controller('dashboardCtrl', function ($scope, $state, $cordovaSocialSharing) {
     localStorage.setItem("isFirstTime", true);
 
     $scope.onClickHospitality = function (Type) {
@@ -9,7 +9,17 @@ LifeLine.controller('dashboardCtrl', function ($scope, $state) {
 
 
     /// Start :: Manage Contact
-    
+
     /// End :: Manage Contact
+
+    /// Start :: Feedback
+
+    /// End :: Feedback
+
+    /// Start :: Share App
+    $scope.onClickShare = function (){
+        $cordovaSocialSharing.share('Hi!, It is nice app for Software Developers', 'InterviewGully', null, 'http://www.interviewgully.com/API/appLauncher.html');
+    };
+    /// End :: Share App
 
 })
